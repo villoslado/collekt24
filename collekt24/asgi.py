a71c8c7d5bcdff71e4d081be461227524f2436dc
Gerry <jg.villoslado@gmail.com>
2024-06-06 15:02:07 -0700

    Project and apps created

---
+++ collekt24/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for collekt24 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "collekt24.settings")
+
+application = get_asgi_application()
